752ff5bacc541c5536982104210942ede592c37d
aspfalex <aspfalex@gmail.com>
2019-03-23 13:46:08 +0100

    boom

--- app/src/main/java/com/example/starwarsencyclopaedia/MainActivity.java
+++ app/src/main/java/com/example/starwarsencyclopaedia/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.starwarsencyclopaedia;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void startSWE(View view){
+        // Create an Intent to start the second activity
+        Intent randomIntent = new Intent(this, SecondActivity.class);
+
+        // Start the new activity.
+        startActivity(randomIntent);
     }
 }
--- app/src/main/java/com/example/starwarsencyclopaedia/SecondActivity.java
+++ app/src/main/java/com/example/starwarsencyclopaedia/SecondActivity.java
@@ -1,13 +1,58 @@
 package com.example.starwarsencyclopaedia;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import com.example.starwarsencyclopaedia.model.Pokemon;
+import com.example.starwarsencyclopaedia.model.RestPokemonResponse;
 
-public class SecondActivity extends AppCompatActivity {
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.app.AppCompatActivity;
+
+import android.widget.TextView;
+import android.util.Log;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+
+public class SecondActivity extends Activity {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+
+    private MainController controller;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        recyclerView = findViewById(R.id.my_recycler_view);
+
+        controller = new MainController(this);
+        controller.onStart();
     }
+
+    public void showList(List<Pokemon> input){
+        recyclerView.setHasFixedSize(true);
+        // use a linear layout manager
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        // define an adapter
+        mAdapter = new MyAdapter(input);
+        recyclerView.setAdapter(mAdapter);
+    }
+
 }
